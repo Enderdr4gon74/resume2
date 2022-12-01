1b27299d0cd9dd7c49593ebd9eaa64cb62796348
Enderdr4gon74 <tristancox03@gmail.com>
2022-11-30 17:47:31 -0700

    first portfolio piece, still needs work though

--- JS/Runner.js
+++ JS/Runner.js
@@ -1,8 +1,8 @@
 if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
   // @ts-ignore
-  document.getElementById("favicon").setAttribute("href", "./assets/img/file-account-white.png")
+  document.getElementById("favicon").setAttribute("href", "https://raw.githubusercontent.com/Enderdr4gon74/resume2/main/assets/Img/file-account-white.png")
 } else {
   // @ts-ignore
-  document.getElementById("favicon").setAttribute("href", "./assets/img/file-account.png")
+  document.getElementById("favicon").setAttribute("href", "https://raw.githubusercontent.com/Enderdr4gon74/resume2/main/assets/Img/file-account.png")
 
 }
